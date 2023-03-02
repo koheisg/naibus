8c72c8f4bcb7397e7c26605e8856bd20809efe9d
Kohei SUGI <koheisg@hey.com>
2023-03-03 01:08:18 +0900

    $ rails g model Workspace workspace_code:string:index access_token:string

---
+++ db/migrate/20230302160811_create_workspaces.rb
@@ -0,0 +1,11 @@
+class CreateWorkspaces < ActiveRecord::Migration[7.0]
+  def change
+    create_table :workspaces do |t|
+      t.string :workspace_code
+      t.string :access_token
+
+      t.timestamps
+    end
+    add_index :workspaces, :workspace_code
+  end
+end
