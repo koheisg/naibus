37a66e5f2ba7e029c06c32193abc1931b47413c3
Kohei SUGI <koheisg@hey.com>
2023-03-04 01:58:38 +0900

    T

--- app/models/slack_service.rb
+++ app/models/slack_service.rb
@@ -5,7 +5,7 @@ class SlackService
     elsif params["event"]["type"] == 'app_mention'
       text = params["event"]["text"].gsub(/<@.*> /, '')
       workspace = Workspace.find_by(workspace_code: params[:team_id])
-      text = OpenAiService.call(text, workspace.openai_api_access_token)
+      text = OpenAiService.call(text, workspace.openai_access_token)
       Slack::Web::Client.new.chat_postMessage(text: text,
                                               thread_ts: params["event"]["ts"],
                                               channel: params["event"]["channel"],
