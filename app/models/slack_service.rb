44fab270b96bf68cf8ee37fa19f2ea0950b9472d
Kohei SUGI <koheisg@hey.com>
2023-03-07 09:45:33 +0900

    Use thread

--- app/models/slack_service.rb
+++ app/models/slack_service.rb
@@ -5,11 +5,13 @@ class SlackService
     elsif params["event"]["type"] == 'app_mention'
       text = params["event"]["text"].gsub(/<@.*> /, '')
       workspace = Workspace.find_by(workspace_code: params[:team_id])
-      text = OpenAiService.call(text, workspace.open_ai_access_token)
-      Slack::Web::Client.new.chat_postMessage(text: text,
-                                              thread_ts: params["event"]["ts"],
-                                              channel: params["event"]["channel"],
-                                              as_user: true)
+      Thread.new do
+        text = OpenAiService.call(text, workspace.open_ai_access_token)
+        Slack::Web::Client.new.chat_postMessage(text: text,
+                                                thread_ts: params["event"]["ts"],
+                                                channel: params["event"]["channel"],
+                                                as_user: true)
+      end
       ''
     else
       ''
