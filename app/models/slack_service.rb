ffb9904746a90304f74de6fe793f3781c5a63608
Kohei SUGI <koheisg@hey.com>
2023-03-09 08:43:50 +0900

    Make cache

--- app/models/slack_service.rb
+++ app/models/slack_service.rb
@@ -6,11 +6,13 @@ class SlackService
       text = params["event"]["text"].gsub(/<@.*> /, '')
       workspace = Workspace.find_by(workspace_code: params[:team_id])
       Thread.new do
-        text = OpenAiService.call(text, workspace.open_ai_access_token)
-        Slack::Web::Client.new.chat_postMessage(text: text,
-                                                thread_ts: params["event"]["ts"],
-                                                channel: params["event"]["channel"],
-                                                as_user: true)
+        Rails.cache.fetch("slack_message_#{params[:event_id]}", expires_in: 1.minute) do
+          text = OpenAiService.call(text, workspace.open_ai_access_token)
+          Slack::Web::Client.new.chat_postMessage(text: text,
+                                                  thread_ts: params["event"]["ts"],
+                                                  channel: params["event"]["channel"],
+                                                  as_user: true)
+        end
       end
       ''
     else
