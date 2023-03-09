fada4075355d341310ce277488db3ddc2bb4dae1
Kohei SUGI <koheisg@hey.com>
2023-03-09 11:31:30 +0900

    ts

--- app/models/slack_service.rb
+++ app/models/slack_service.rb
@@ -7,20 +7,24 @@ class SlackService
       workspace = Workspace.find_by(workspace_code: params[:team_id])
       Thread.new do
         Rails.cache.fetch("slack_message_#{params[:event][:client_msg_id]}", expires_in: 1.minute) do
-          ChatThread.find_or_create_by(message_code: params[:event][:client_msg_id], ts_code: params["event"]["ts"], message: text, role: :user)
+          thread_ts = params["event"]["thread_ts"] || params["event"]["ts"]
+          ChatThread.find_or_create_by(message_code: params[:event][:client_msg_id], role: :user) do |chat_thread|
+            chat_thread.ts_code = thread_ts
+            chat_thread.message = text
+          end
 
           Slack.configure do |config|
             config.token = workspace.access_token
           end
 
-          messages = ChatThread.where(ts_code: params["event"]["ts"]).map { |chat_thread| { role: chat_thread.role.to_s, content: chat_thread.message } }
+          messages = ChatThread.where(ts_code: thread_ts).map { |chat_thread| { role: chat_thread.role.to_s, content: chat_thread.message } }
           text = OpenAiService.call(messages, workspace.open_ai_access_token)
           Slack::Web::Client.new.chat_postMessage(text: text,
                                                   thread_ts: params["event"]["ts"],
                                                   channel: params.dig("event","channel"),
                                                   as_user: true)
 
-          ChatThread.find_or_create_by(message_code: params[:event][:client_msg_id], ts_code: params["event"]["ts"], message: text, role: :assistant)
+          ChatThread.find_or_create_by(message_code: params[:event][:client_msg_id], ts_code: thread_ts, message: text, role: :assistant)
         end
       end
       ''
