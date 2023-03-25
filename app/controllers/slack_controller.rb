64c5c962486aeb52959a8fed049174a624986783
Kohei SUGI <koheisg@hey.com>
2023-03-25 12:41:06 +0900

    fix

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -16,9 +16,9 @@ class SlackController < ActionController::API
       render json: '', status: 200
     elsif params["event"]["type"] == 'reaction_added' && params["event"]["reaction"] == 'naibus'
       workspace = Workspace.find_by!(workspace_code: params['authorizations'][0]['team_id'])
-      SlackRetriveConversationJob.perform_now(workspace,
-                                              params['event']['item']['channel'],
-                                              params['event']['item']['ts'])
+      SlackRetriveConversationJob.perform_later(workspace,
+                                                params['event']['item']['channel'],
+                                                params['event']['item']['ts'])
       render json: '', status: 200
     else
       render json: '', status: 200
