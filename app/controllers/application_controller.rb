36d6c36f915921c21f65b14d02c07cf3f453991f
Kohei SUGI <koheisg@hey.com>
2023-05-07 22:21:43 +0900

    Add user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  helper_method :current_user
+
   def current_workspace
     @current_workspace ||= Workspace.find_by(id: session[:workspace_id])
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_07_025309) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_07_132133) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_07_025309) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["chat_thread_id"], name: "index_ref_urls_on_chat_thread_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "workspaces", force: :cascade do |t|
