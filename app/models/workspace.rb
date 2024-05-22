248021e0e2757432c8ba7bcebcae34c8edd1e714
Kohei SUGI <koheisg@hey.com>
2024-05-22 19:15:16 +0900

    Update selectable model

--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -3,5 +3,5 @@
 class Workspace < ApplicationRecord
   validates :workspace_code, presence: true, uniqueness: true
   validates :access_token, presence: true
-  enum open_ai_model: %i[gpt-3.5-turbo gpt-4 gpt-4-1106-preview	code-davinci-002	text-davinci-003]
+  enum open_ai_model: %i[gpt-4-turbo gpt-4o]
 end
